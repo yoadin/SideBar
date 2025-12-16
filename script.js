c05fec35370c585007be065023cb34d9d79dafd2
yoadin <kalkidanasdro11@gmail.com>
2025-12-16 23:00:11 +0300

    method for open button added

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const closeBtn = document.getElementById("closeSidebarBtn");
+const openBtn = document.getElementById("openSidebarBtn");
 
 closeBtn.addEventListener("click", () => {
     document.querySelector(".sidebar").style.right = "-250px";
 });
+
+openBtn.addEventListener("click", () => {
+    document.querySelector(".sidebar").style.right = "0";
+});
+
+
 // Write JavaScript code here
