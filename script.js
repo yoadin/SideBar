844b03c8eb7db1ad3365045d414f12f72fb706a3
yoadin <kalkidanasdro11@gmail.com>
2025-12-16 22:56:39 +0300

    Javascript added to so close side bar button can work

---
+++ script.js
@@ -0,0 +1,6 @@
+const closeBtn = document.getElementById("closeSidebarBtn");
+
+closeBtn.addEventListener("click", () => {
+    document.querySelector(".sidebar").style.right = "-250px";
+});
+// Write JavaScript code here
